6e4c23021cc4972ccbbfba5c54150208cb648fda
James Paul Mason <jmason86@gmail.com>
2017-10-06 16:51:21 -0400

    Initial commit

---
+++ HeaderTemplate.py
@@ -0,0 +1,31 @@
+#+
+# NAME: 
+#   HeaderTemplate
+#   
+# PURPOSE:
+#   
+#   
+# INPUTS: 
+#   
+#
+# OPTIONAL INPUTS:
+#   
+#
+# Flags:
+#   
+#
+# OUTPUTS:
+#   
+#
+# OPTIONAL OUTPUTS: 
+#   
+#
+# RESTRICTIONS:
+#   
+#   
+# EXAMPLE: 
+#   
+#   
+# MODIFICATION HISTORY: 
+#   2015-12-23: James Paul Mason: Wrote script.
+#-
