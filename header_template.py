be6ce9389044d6c3b02ad50dd909744bef6993f6
James Paul Mason <jmason86@gmail.com>
2017-10-10 16:10:50 -0400

    Updated to conform to PEP8

---
+++ header_template.py
@@ -0,0 +1,30 @@
+# Standard modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Custom modules
+
+__author__ = 'James Paul Mason'
+__contact__ = 'jmason86@gmail.com'
+
+
+def function_name():
+    """Convert seconds of day to 'hh:mm:ss'
+
+    Inputs:
+        sod [np.array]: The array of seconds of day to convert.
+
+    Optional Inputs:
+        None
+
+    Outputs:
+        hhmmss [np chararray]: Array of strings of the form 'hh:mm:ss'
+
+    Optional Outputs:
+        None
+
+    Example:
+        hhmmss = sod_to_hhmmss(sod)
+    """
+    return 1
